574aa6a75663970600fea6b3b358ff2822b41ed5
Liam <lcain0493@conestogac.on.ca>
2023-06-11 14:08:02 -0400

    the buttons are now connected to an animation, but the animation needs fleshed out

--- elevatorControl.js
+++ elevatorControl.js
@@ -1,38 +1,44 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Elevator object
+    const elevatorOC = {
+        isOpen: false,
 
-// Elevator object
-const elevator = {
-    isOpen: false,
+        openDoor: function() {
+        if (!this.isOpen) {
+            this.isOpen = true;
+            const doors = document.querySelectorAll(".elevator-door");
+            doors.forEach(function(doors) {
+                // Perform operations on each element
+                doors.classList.add("open");
+                doors.classList.remove("close");
+            });
+        }
+        },
 
-    openDoor: function() {
-    if (!this.isOpen) {
-        this.isOpen = true;
-        document.getElementByClass("elevatorDoor").classList.add("open");
-        document.getElementByClass("elevatorDoor").classList.remove("close");
-    } else {
-    }
-    },
-
-    closeDoor: function() {
-    if (this.isOpen) {
-        this.isOpen = false;
-        document.getElementByClass("elevatorDoor").classList.add("close");
-        document.getElementByClass("elevatorDoor").classList.remove("open");
-    } else {
-    }
-    }
-};
-
-// Get references to the buttons
-var openBtn = document.getElementById("openButton");
-var closeBtn = document.getElementById("closeButton");
-
-// Attach click event listeners to the buttons
-window.onload=function(){
+        closeDoor: function() {
+        if (this.isOpen) {
+            this.isOpen = false;
+            const doors = document.querySelectorAll(".elevator-door");
+            doors.forEach(function(doors) {
+                // Perform operations on each element
+                doors.classList.remove("open");
+                doors.classList.add("close");
+            });
+        }
+        }
+    };
+    var openBtn = document.querySelector("#openButton");
+    var closeBtn = document.querySelector("#closeButton");
+    // Attach click event listeners to the buttons
     openBtn.addEventListener("click", function() {
-        elevator.openDoor();
+        elevatorOC.openDoor();
+    });
+    
+    closeBtn.addEventListener("click", function() {
+        elevatorOC.closeDoor();
     });
 
-    closeBtn.addEventListener("click", function() {
-        elevator.closeDoor();
-    });
-}
+});
+
+
+
--- indicatorScript.js
+++ indicatorScript.js
@@ -1,17 +1,19 @@
-// Just random for now
+document.addEventListener("DOMContentLoaded", function(){
+    // Just random for now
 
-let x = Math.floor((Math.random() * 3) + 1);
-let floorIcon = document.getElementById('FloorNum');
+    let x = Math.floor((Math.random() * 3) + 1);
+    let floorIcon = document.getElementById('FloorNum');
 
-if ( x == 1){
-   floorIcon.id='Circle1';
-}
-if ( x == 2){
-    floorIcon.id='Circle2';
-}
-if ( x == 3){
-    floorIcon.id='Circle3';
-}
+    if ( x == 1){
+    floorIcon.id='Circle1';
+    }
+    if ( x == 2){
+        floorIcon.id='Circle2';
+    }
+    if ( x == 3){
+        floorIcon.id='Circle3';
+    }
+});
 
 
 
