2f510028553268e8fddd77b6b0a13d0a63cc203d
SquishedCat <cainl3758@outlook.com>
2023-06-11 16:56:35 -0400

    Doors move correctly now. Images moved to folder. Yay

--- elevatorControl.js
+++ elevatorControl.js
@@ -11,6 +11,10 @@ document.addEventListener("DOMContentLoaded", function() {
                 // Perform operations on each element
                 doors.classList.add("open");
                 doors.classList.remove("close");
+                //waits for 2 seconds
+                setTimeout(function(){
+                    openButtonCheckbox.checked = false;
+                },2000);
             });
         }
         },
@@ -23,12 +27,18 @@ document.addEventListener("DOMContentLoaded", function() {
                 // Perform operations on each element
                 doors.classList.remove("open");
                 doors.classList.add("close");
+                //waits for 2 seconds
+                setTimeout(function(){
+                    closeButtonCheckbox.checked = false;
+                },2000);
             });
         }
         }
     };
     var openBtn = document.querySelector("#openButton");
     var closeBtn = document.querySelector("#closeButton");
+    var openButtonCheckbox = document.querySelector("#jsOpenButton");
+    var closeButtonCheckbox = document.querySelector("#jsCloseButton");
     // Attach click event listeners to the buttons
     openBtn.addEventListener("click", function() {
         elevatorOC.openDoor();
