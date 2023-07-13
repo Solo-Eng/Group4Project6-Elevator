0875f58294b7e1759f794932ad576c1d6a1626f7
Liam <lcain0493@conestogac.on.ca>
2023-07-13 08:15:44 -0400

    8

--- index.php
+++ index.php
@@ -28,4 +28,12 @@
 			return $current_floor;
 	}
 ?>
+<?php
+    if(isset($_POST['newfloor'])) {
+      $curFlr = update_elevatorNetwork(1, $_POST['newfloor']); 
+      header('Refresh:0; url=index.php');	
+    } 
+    $curFlr = get_currentFloor();
+    echo json_encode($curFlr);			
+  ?>		
 
--- indicatorScript.js
+++ indicatorScript.js
@@ -88,3 +88,22 @@ function submitNumber2(){
 function submitNumber3(){
 	document.getElementById("floorNumber3").submit();
 }
+
+function showFloorInterval() {    // Automatic updates every 250 ms
+	setInterval(showFloor, 5000);
+}
+
+function showFloor() {
+	var xmlhttpShow = new XMLHttpRequest();     // Instantiate a XMLHttpRequest object
+	// Function to be executed when readyState changes (server response ready)
+	xmlhttpShow.onreadystatechange = function() {
+		if(this.readyState == 4 && this.status == 200) {
+		// readyState holds the status of the XMLHttpRequest (4 means finished request and server response is ready)
+		// status hold 200 for OK
+
+		} };
+		xmlhttpShow.open("GET", "index.php?q=", true);  // Open connection
+		xmlhttpShow.send();                                       // Send request
+		
+}
+window.addEventListener('load', function() {showFloorInterval(), false});
