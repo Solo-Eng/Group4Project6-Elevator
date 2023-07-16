afbd55e7c50b5a5baa0022efa64222fafc47cdae
Liam <lcain0493@conestogac.on.ca>
2023-07-16 15:07:34 -0400

    tried putting it in a different file - maybe it's cause I hard set the updte value?

--- php/floorUpdate.php
+++ php/floorUpdate.php
@@ -1,9 +1,9 @@
 
 <?php
-    if(isset($_POST['newfloor'])) {
-      $curFlr = update_elevatorNetwork(1, $_POST['newfloor']); 
-      header('Refresh:0; url=index.php');	
-    } 
-    $curFlr = get_currentFloor();
+  if(isset($_POST['newfloor'])) {
+    $curFlr = update_elevatorNetwork(2, $_POST['newfloor']); 
+    header('Refresh:0; url=index.php');	
+  } 
+  $curFlr = get_currentFloor();
   echo json_encode($curFlr);	
 ?>
