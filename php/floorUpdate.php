52cf6a55d3fd4183aaef9462bcc83ff4b44d1dfd
Liam <lcain0493@conestogac.on.ca>
2023-07-16 15:01:46 -0400

    tried putting it in a different file - tried getting rid of the hidden class

--- php/floorUpdate.php
+++ php/floorUpdate.php
@@ -1,11 +1,9 @@
 
-<div class = hidden>
-    <?php
-      if(isset($_POST['newfloor'])) {
-        $curFlr = update_elevatorNetwork(1, $_POST['newfloor']); 
-        header('Refresh:0; url=index.php');	
-      } 
-      $curFlr = get_currentFloor();
-	  echo json_encode($curFlr);	
-    ?>		
-</div>	
+<?php
+    if(isset($_POST['newfloor'])) {
+      $curFlr = update_elevatorNetwork(1, $_POST['newfloor']); 
+      header('Refresh:0; url=index.php');	
+    } 
+    $curFlr = get_currentFloor();
+  echo json_encode($curFlr);	
+?>
