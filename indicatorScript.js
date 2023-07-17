fadccbcd13786436eb5d36d81a6a8e44dd090cb8
Liam <lcain0493@conestogac.on.ca>
2023-07-17 13:30:51 -0400

    buttons went green and stayed green - tried reordering the function in submit 1

--- indicatorScript.js
+++ indicatorScript.js
@@ -80,8 +80,8 @@ f1 = document.getElementById('floor3Select');
 f1.addEventListener('click',floor3,false);
 
 function submitNumber1(){
-	document.getElementById("floorNumber1").submit();
 	floor1();
+	document.getElementById("floorNumber1").submit();
 }
 function submitNumber2(){
 	document.getElementById("floorNumber2").submit();
