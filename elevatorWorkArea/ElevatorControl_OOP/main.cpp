1bcc46a2a0d50f2105b70fcedadee144129ede76
eagle-eye-0014 <ballen3456@conestogac.on.ca>
2023-07-19 21:41:24 -0400

    added my updated main.cpp

--- elevatorWorkArea/ElevatorControl_OOP/main.cpp
+++ elevatorWorkArea/ElevatorControl_OOP/main.cpp
@@ -50,10 +50,13 @@ int main() {
 				while(1){
 					// Take the requested Floor from the database			
 					floorNumArray[0] = db_getFloorNum();	
+
+					dist = getDistance();
 					
 					// Calculate the difference between 2nd and 3rd positions
 					diff1 = floorNumArray[2] - floorNumArray[1];
 					diff1 = abs(diff1);
+					
 										
 					// a printf for debugging
 					printf("\n%d %d %d\n",floorNumArray[0],floorNumArray[1],floorNumArray[2]);
@@ -70,7 +73,7 @@ int main() {
 							
 
 
-							dist = getDistance();
+							
 							if(diff2 > 0){
 								//going up
 								if(dist < 1000){
@@ -104,7 +107,9 @@ int main() {
 								// Wait for new passengers
 								// Continue with request
 								sleep(2);								
-								pcanTx(ID_SC_TO_EC, HexFromFloor(floorNumArray[1]));						
+								pcanTx(ID_SC_TO_EC, HexFromFloor(floorNumArray[1]));	
+								sleep(1);	
+								db_setFloorNum(floorNumArray[1]);				
 
 								// make sure tf = 0;
 								tf = 0;	
@@ -120,7 +125,18 @@ int main() {
 					
 					// Set the database
 					// Will set currentFloor in the database
-					db_setFloorNum(floorNumArray[0]);
+					
+					// EXPERIMENTAL //					
+					if(dist > 400 && dist < 600){
+						db_setFloorNum(1);
+					}
+					else if(dist > 700 && dist < 800){
+						db_setFloorNum(2);
+					}
+					else if(dist >1450 && dist < 1600){
+						db_setFloorNum(3);
+					}
+					
 
 					// shift the queue
 					floorNumArray[2] = floorNumArray[1];
