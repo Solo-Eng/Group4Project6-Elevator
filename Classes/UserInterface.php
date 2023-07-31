54295e5ce5c2e9502b5542552acf0d59a7ec9913
Liam <lcain0493@conestogac.on.ca>
2023-07-31 10:08:15 -0400

    Added exceptions

--- Classes/UserInterface.php
+++ Classes/UserInterface.php
@@ -19,16 +19,29 @@
 
         function updateRequestedFloor($nodeID, $requestedFloor){
 
-            $this->requestFloor = $requestedFloor;
-            $db1 = new PDO('mysql:host=127.0.0.1;dbname=elevator','ese','ese');
-		    $query =   'UPDATE elevatorNetwork 
-				        SET requestedFloor = :floor
-				        WHERE nodeID = :id';
-		    $statement = $db1->prepare($query);
-		    $statement->bindvalue('floor', $new_floor);
-		    $statement->bindvalue('id', $node_ID);
-		    $statement->execute();	
-            echo "<script>console.log('" . $new_floor . "');</script>";
+			try {
+				if ($nodeID < 0){
+					throw new Exception('node_ID must be positive');
+				}
+				else if($requestedFloor < 0 || $requestedFloor > 3){
+					throw new Exception('requested floor must be between 1 and 3');
+				}
+				else {
+					$this->requestFloor = $requestedFloor;
+					$db1 = new PDO('mysql:host=127.0.0.1;dbname=elevator','ese','ese');
+					$query =   'UPDATE elevatorNetwork 
+								SET requestedFloor = :floor
+								WHERE nodeID = :id';
+					$statement = $db1->prepare($query);
+					$statement->bindvalue('floor', $new_floor);
+					$statement->bindvalue('id', $node_ID);
+					$statement->execute();	
+					echo "<script>console.log('" . $new_floor . "');</script>";
+				}
+			}
+            catch (Exception $e){
+				echo e->getMessage();
+			}
 		    return $this->requestFloor;
         }
 
